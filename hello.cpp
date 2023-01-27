fc4ee6a8ed5dcc27de1a1175bf05bbd51d048bbc
Jeremy Bergen <jbergen@coloradomesa.edu>
2023-01-27 08:19:54 -0700

    Our first hello world!

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+/*
+Name: Jeremy Bergen
+
+First hello world program
+*/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    //This will print out hello world to the console
+    cout << "Hello World!" << endl;
+
+    return 0;
+}
