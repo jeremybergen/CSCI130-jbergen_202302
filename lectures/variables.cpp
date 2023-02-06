6284c58b38e43b2be635b73e7143cce494c14d8a
Jeremy Bergen <jbergen@coloradomesa.edu>
2023-02-06 13:51:24 -0700

    Monday 0206

---
+++ lectures/variables.cpp
@@ -0,0 +1,95 @@
+/*
+Name: Jeremy Bergen
+
+All about the variables
+*/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num1, num2;
+
+    cout << "Please enter two numbers separated by a space: ";
+    cin >> num1 >> num2;
+
+    cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+    cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
+    cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+    cout << num1 << " / " << num2 << " = " << num1 / num2 << endl;
+
+    // string line3;
+
+    // line3 = "This is a s\tentence.";
+    // cout << line3 << endl;
+    // line3 = "This is a s-\tentence.";
+    // cout << line3 << endl;
+    // line3 = "This is a s--\tentence.";
+    // cout << line3 << endl;
+    // line3 = "This is a s---\tentence.";
+    // cout << line3 << endl;
+    // line3 = "This is a s----\tentence.";
+    // cout << line3 << endl;
+
+    // string firstName, lastName, fullName;
+
+    // // cout << "Please enter your full name with a space: ";
+    // // cin >> firstName >> lastName;
+
+    // cout << "Please enter you first name: ";
+    // // cin >> firstName;
+    // getline(cin, firstName);
+
+    // cout << "Please enter your last name: ";
+    // // cin >> lastName;
+    // getline(cin, lastName);
+
+    // // Option 1
+    // // fullName = firstName + " " + lastName;
+    // // fullName = "Welcome " + firstName + " " + lastName;
+    // // cout << "Welcome " << fullName << endl;
+
+    // // Option 2
+    // // cout << "Welcome " << firstName + " " + lastName << endl;
+
+    // // Option 3
+    // cout << "Welcome " + firstName + " " + lastName << endl;
+
+    // int number1;
+    // int number2;
+    // int total;
+
+    // cout << "Please enter a whole number: ";
+    // cin >> number1;
+
+    // cout << "The value of number1 is: " << number1 << endl;
+
+    // cout << "Please enter a whole number: ";
+    // cin >> number2;
+    
+    // cout << "The value of number2 is: " << number2 << endl;
+
+    // total = number1 + number2;
+
+    // cout << "The total of number1 + number2 is: " << total << endl;
+
+    // number1 = 42;
+
+    // cout << "The address of number1 is: " << &number1 << endl;
+    // cout << "The value of number1 is: " << number1 << endl;
+
+    // number1 = -9000;
+
+    // cout << "The address of number1 is: " << &number1 << endl;
+    // cout << "The value of number1 is: " << number1 << endl;
+
+    // number2 = 24;
+
+    // cout << "The value of number2 is: " << number2 << endl;
+
+    // total = number1 + number2;
+
+    // cout << "The sum of number1 and number2 is: " << total << endl;
+
+    return 0;
+}
