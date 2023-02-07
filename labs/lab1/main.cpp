11a456b71375b3860e9e82caad5e7a81cf98969a
Jeremy Bergen <jbergen@coloradomesa.edu>
2023-02-07 13:26:02 -0700

    fixed fixme2 and 4 in lab1

--- labs/lab1/main.cpp
+++ labs/lab1/main.cpp
@@ -3,7 +3,7 @@
     ASCII Art - using literals and variables
     
     Updated By: Jeremy Bergen #FIXME1 fixed
-    Date: ... #FIXME2
+    Date: 02/07/23 #FIXME2 fixed
     
     This program produces an ASCII art on the console.
     Algorithm steps: 
@@ -22,6 +22,8 @@ int main()
     //FIXME3: declare a variable to store name
     // fixed
     //FIXME4: prompt user to enter their name and store the value in variable
+    // fixed
+    //I will prompt for the name...
     
     //FIXME5: greet the name using the variable as the following output
     //must output: Nice meeting you, <name>!
