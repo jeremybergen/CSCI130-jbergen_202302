dba191ab2fb46b9a86389a7700d1f61de7895fb2
Jeremy Bergen <jbergen@coloradomesa.edu>
2023-04-04 13:51:23 -0600

    tuesday 0404

---
+++ lectures/randomNumbers.cpp
@@ -0,0 +1,33 @@
+/*
+Name: Jeremy Bergen
+
+Random Numbers!!!
+*/
+#include <iostream>
+#include <random>
+#include <unistd.h>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    cout << "time(0): " << time(0) << endl;
+    sleep(1);
+    cout << "time(0): " << time(0) << endl;
+    sleep(1);
+    cout << "time(0): " << time(0) << endl;
+    sleep(1);
+    cout << "time(0): " << time(0) << endl;
+    sleep(1);
+    cout << "time(0): " << time(0) << endl;
+    sleep(1);
+    // int counter = 0;
+
+    // srand(time(0));
+    
+    // while(counter < 10) {
+    //     cout << "random[" << counter << "]: " << random() << endl;
+    //     counter++;
+    // }
+
+    return 0;
+}
