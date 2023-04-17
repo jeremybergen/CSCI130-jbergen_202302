bfea1f798f7a95b7ae2727298eccff8de3309962
Jeremy Bergen <jbergen@coloradomesa.edu>
2023-04-17 13:39:09 -0600

    monday 0417

--- lectures/linkedList.cpp
+++ lectures/linkedList.cpp
@@ -10,30 +10,46 @@ using namespace std;
 
 struct Node {
     int data;
-    Node *next;
+    Node *head;
+    Node *tail;
 };
+
+void llSize(int&);
+void buildList(Node *, Node *);
+void printList(Node *);
+void deleteList(Node *);
 
 int main(int argc, char *argv[]) {
     int listSize = 0;
-    Node *first = new Node({0, NULL});
+    Node *first = new Node();
     Node *curNode;
 
-
-    cout << "How many elements do you want in your list? ";
-    cin >> listSize;
+    llSize(listSize);
 
     curNode = first;
     for(int i = 0; i < listSize; i++) {
-        Node *newNode = new Node();
+        if (i < listSize-1) {
+            Node *newNode = new Node();
+            buildList(curNode, newNode);
+            curNode = newNode;
+        } else {
+            buildList(curNode, first);
+        }
 
-        cout << "Enter a number: ";
-        cin >> newNode->data;
-        newNode->next = NULL;
-
-        curNode->next = newNode;
-        // curNode = newNode;
-        curNode = curNode->next;
+        // buildList(first, NULL);
+        // if (i == 0) {
+        //     buildList(first, NULL);
+        //     curNode = first;
+        // } else {
+        //     Node *newNode = new Node();
+        //     buildList(curNode, newNode);
+        //     curNode = newNode;
+        // }
     }
+    
+
+    printList(first);
+    deleteList(first);
 
     // Node *second = new Node();
     // Node *third = new Node();
@@ -55,23 +71,71 @@ int main(int argc, char *argv[]) {
     // cout << "second->data: " << second->data << endl;
     // cout << "third->data: " << third->data << endl;
 
-    curNode = first;
-    while(curNode != NULL) {
-        cout << "curNode->data: " << curNode->data << endl;
-        curNode = curNode->next;
-    }
 
-    curNode = first;
-    while(curNode != NULL) {
-        Node *deleteNode;
-        deleteNode = curNode;
-        curNode = curNode->next;
-        delete deleteNode;
-    }
+
 
     // delete first;
     // delete second;
     // delete third;
     // delete fourth;
     return 0;
+}
+
+void deleteList(Node *first) {
+    // Node *curNode;
+
+    // curNode = first;
+    while(first != NULL) {
+        Node *deleteNode;
+        deleteNode = first;
+        first = first->tail;
+        delete deleteNode;
+    }
+}
+
+void printList(Node *first) {
+    // Node *curNode;
+
+    // curNode = first;
+    while(first != NULL) {
+        cout << "first->data: " << first->data << endl;
+        first = first->tail;
+    }
+
+}
+
+void buildList(Node *node, Node *nextNode) {
+    cout << "Please enter a number: ";
+    cin >> node->data;
+    node->tail = nextNode;
+    nextNode->head = node;
+
+    // Node *curNode;
+
+    // curNode = first;
+    
+
+    // for(int i = 0; i < listSize; i++) {
+    //     // if(i == 0) {
+    //     //     cout << "Enter a number: ";
+    //     //     cin >> first->data;
+    //     //     first->next = NULL;
+    //     // } else {
+    //     Node *newNode = new Node();
+
+    //     cout << "Enter a number: ";
+    //     cin >> newNode->data;
+    //     newNode->next = NULL;
+
+    //     first->next = newNode;
+    //     // curNode = newNode;
+    //     first = first->next;
+    //     // }
+    // }
+
+}
+
+void llSize(int &listSize) {
+    cout << "How many elements do you want in your list? ";
+    cin >> listSize;
 }
